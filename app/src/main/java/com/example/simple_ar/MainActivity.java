0b9623b6049c020ef77da8a2b5119981c54dff2a
hritikgupta <hritikgupta9@gmail.com>
2020-12-19 15:24:19 +0530

    initial commit: dog renderer
    Signed-off-by: hritikgupta <hritikgupta9@gmail.com>

---
+++ app/src/main/java/com/example/simple_ar/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.simple_ar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.HitTestResult;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
+
+    private ArFragment arFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+        arFragment.setOnTapArPlaneListener(
+                (HitResult hitRes, Plane plane, MotionEvent motionEv) -> {
+                    Anchor anchor = hitRes.createAnchor();
+                    placeObject(arFragment, anchor, Uri.parse("model.sfb"));
+                });
+    }
+
+    private void placeObject(ArFragment arFragment, Anchor anchor, Uri uri  ){
+        ModelRenderable.builder()
+                .setSource(arFragment.getContext(), uri)
+                .build()
+                .thenAccept(modelRenderable -> addNodeToScene(arFragment, anchor, modelRenderable))
+                .exceptionally(throwable -> {
+                            Toast.makeText(arFragment.getContext(), "Error:" + throwable.getMessage(), Toast.LENGTH_LONG).show();
+                            return null;
+                        }
+                );
+    }
+
+    private void addNodeToScene(ArFragment arFragment, Anchor anchor, Renderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(arFragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        arFragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+
+
+}
